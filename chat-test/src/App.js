3f78496be6558c0c60bec3e197b767983d062820
cvivis <hsy0754@naver.com>
2023-10-27 17:50:03 +0900

    [Feat] : 파일 읽은 text console.log() 확인 코드 추가

---
+++ chat-test/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import LogCheck from './logCheck';
+function App() {
+  return (
+    <div>
+      <div>
+      <LogCheck></LogCheck>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
+
---
+++ chat-test/src/logCheck.js
@@ -0,0 +1,63 @@
+import './App.css';
+import { useState , useRef, useEffect} from 'react';
+import * as StompJs from '@stomp/stompjs';
+
+
+
+function LogCheck(){
+
+/*stomp 관련 */
+  const client = useRef({}); 
+  const [log,setLog] = useState("");
+  const connect = () =>{
+      client.current = new StompJs.Client({
+          brokerURL: 'ws://localhost:8080/ws/monitoring',
+          onConnect:() =>{
+             // Do something, all subscribes must be done is this callback
+            console.log("연결 SUB");
+              subscribe();
+          },
+      });
+      client.current.activate();
+  }
+  useEffect(() => {
+    connect(); // 마운트시 실행
+    return () => disconnect(); // 언마운트 시 실행
+  },[]);
+
+
+  const disconnect = () => {
+      client.current.deactivate(); // 활성화된 연결 끊기 
+  };
+
+  const subscribe = () => {
+      client.current.subscribe('/sub/log', (res) => { // server에게 메세지 받으면
+        // console.log(res)
+        // const json_body = JSON.parse(res.body);
+        // console.log(json_body);
+        // setLog(json_body);
+        console.log(res.body);
+      });
+    };
+
+    // const publish = () =>{
+    //   client.current.publish({
+    //     destination: '/pub/normal/' + applyId,
+    //     body:JSON.stringify( {id:applyId, price:myPrice.current , memberId:1, itemId:1}),
+    //     skipContentLengthHeader: true,
+    //   });
+      
+    // }
+
+
+
+  return (
+    <div className="App">
+      <p>log : {log}</p>
+      <p>---------------------------------</p>
+    </div>
+  );
+
+}
+
+export default LogCheck;
